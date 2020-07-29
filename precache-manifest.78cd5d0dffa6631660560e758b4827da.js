ea6e0eec8a7c3cadfcc0ff8007d8a84da1d3e636
samar08 <samarasimhareddy.mohan@gmail.com>
2020-07-29 16:44:36 +0530

    Updates

---
+++ precache-manifest.78cd5d0dffa6631660560e758b4827da.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6cf6983c6053c279f61f695e8a0e93b5",
+    "url": "/Demoportfolio/index.html"
+  },
+  {
+    "revision": "6486ef5ed92f896aa71a",
+    "url": "/Demoportfolio/static/css/main.cf80c280.chunk.css"
+  },
+  {
+    "revision": "37aeebcf6feab626ea1d",
+    "url": "/Demoportfolio/static/js/2.0b6c0ac6.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Demoportfolio/static/js/2.0b6c0ac6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6486ef5ed92f896aa71a",
+    "url": "/Demoportfolio/static/js/main.ef7586eb.chunk.js"
+  },
+  {
+    "revision": "59f5a99c905304a4012a",
+    "url": "/Demoportfolio/static/js/runtime-main.73d74ebf.js"
+  }
+]);
